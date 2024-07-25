9e3e68922466ac8a16ffc670dc3642b4938e2676
kingzamzon <adesanoye.samson1@gmail.com>
2024-07-25 08:45:25 -0700

    fix transfer using lowercase fromPubkey

---
+++ README.md
@@ -0,0 +1,24 @@
+## Advice
+
+Do anything defi 
+
+Go Hard or Ho Home
+
+## Wallet address:
+
+```
+FScyM7Utyv6pF8dPRhCDSH3V88Z8MxPagpbHJsLgBuF1
+```
+
+## Airdrop transaction
+
+[2NxFejFFprjcxrGatgCmStE8ibEK5uHsr4o9qDhBXjtxTo7rBXEKCun5rm36mz7Jcj3YayjE9bDep6UD7PXLoemu](https://solscan.io/tx/2NxFejFFprjcxrGatgCmStE8ibEK5uHsr4o9qDhBXjtxTo7rBXEKCun5rm36mz7Jcj3YayjE9bDep6UD7PXLoemu?cluster=devnet)
+
+
+## Transfer transaction
+
+[V22T9xPtVUENcCmQVnzHxnjTQceeAFN8vAMzk1wxgGzqiRKzRhRKKZqoXtxLn1RFBbYrinYzBQ7MY3UKAK9Kj2R](https://solscan.io/tx/V22T9xPtVUENcCmQVnzHxnjTQceeAFN8vAMzk1wxgGzqiRKzRhRKKZqoXtxLn1RFBbYrinYzBQ7MY3UKAK9Kj2R?cluster=devnet)
+
+## Transfer to Wallet
+
+[4E9b2RaZFs1eKYCXriJSFwmkp4ghyXdNpapKVnTgU7B1](https://solscan.io/account/4E9b2RaZFs1eKYCXriJSFwmkp4ghyXdNpapKVnTgU7B1?cluster=devnet)
--- transfer.js
+++ transfer.js
@@ -1,12 +1,75 @@
-import { Connection, Keypair, PublicKey } from "@solana/web3.js"
-import walletKey from "./wallet.json" assert {type: "json"}
+import {
+  Connection,
+  Keypair,
+  PublicKey,
+  sendAndConfirmTransaction,
+  SystemProgram,
+  Transaction,
+  clusterApiUrl
+} from "@solana/web3.js";
+import walletKey from "./wallet.json" assert { type: "json" };
 
+const to = new PublicKey("4E9b2RaZFs1eKYCXriJSFwmkp4ghyXdNpapKVnTgU7B1");
+const from = Keypair.fromSecretKey(new Uint8Array(walletKey));
 
-const to = new PublicKey("4E9b2RaZFs1eKYCXriJSFwmkp4ghyXdNpapKVnTgU7B1")
-const from = Keypair.fromSecretKey(new Uint8Array(walletKey))
+const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
 
-
-console.log(to, from)
+console.log(to, from);
 // solana is a stepper
 // transaction to instruction to program
-// program are executable. 
+// program are executable.
+// everything must be successful for it to be valid
+const transfer = async () => {
+  const balance = await connection.getBalance(from.publicKey);
+  console.log(balance);
+
+  if (balance === 0) {
+    // write a function that tops the balance if it not enough
+
+    console.log("Oga you no have money");
+  } else {
+    // Look in for capitaliztion
+    const transaction = new Transaction().add(
+      SystemProgram.transfer({
+        fromPubkey: from.publicKey,
+        toPubkey: to,
+        lamports: balance,
+      })
+    );
+    transaction.feePayer = from.publicKey;
+    
+    const { blockhash } = await connection.getLatestBlockhash("confirmed");
+    transaction.recentBlockhash = blockhash;
+
+    const fee =
+      (
+        await connection.getFeeForMessage(
+          transaction.compileMessage(),
+          "confirmed"
+        )
+      ).value || 0;
+
+    console.log(fee);
+
+    // the first transaction is removed
+    transaction.instructions.pop();
+
+    transaction.add(
+      SystemProgram.transfer({
+        fromPubkey: from.publicKey,
+        toPubkey: to,
+        lamports: balance * 0.1 - fee,
+      })
+    );
+
+    // we add the transaction first so solana can use it
+    // for the cost of the transaction
+    // take your transaction as class that has properties
+    const send = await sendAndConfirmTransaction(connection, transaction, [
+      from,
+    ]);
+    console.log(send);
+  }
+};
+
+transfer();
